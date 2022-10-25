2d972b9cd389e4c067bd8c24f70a1111fc970027
FathurRahmanRais <83505914+FathurRahmanRais@users.noreply.github.com>
2022-10-25 19:24:40 +0700

    Update Model_arsip.php

--- app/Models/Model_arsip.php
+++ app/Models/Model_arsip.php
@@ -10,6 +10,7 @@ class Model_arsip extends Model
     {
         return $this->db->table('tbl_arsip')
         ->join('tbl_dep', 'tbl_dep.id_dep = tbl_arsip.id_dep', 'left')
+        ->join('tbl_user', 'tbl_user.id_user = tbl_arsip.id_user', 'left')
         ->join('tbl_kategori', 'tbl_kategori.id_kategori = tbl_arsip.id_kategori', 'left')
         ->orderBy('id_arsip','DESC')
         ->get()
@@ -20,8 +21,9 @@ class Model_arsip extends Model
     {
         $this->db->table('tbl_arsip')
         ->join('tbl_dep', 'tbl_dep.id_dep = tbl_arsip.id_dep', 'left')
+        ->join('tbl_user', 'tbl_user.id_user = tbl_arsip.id_user', 'left')
         ->join('tbl_kategori', 'tbl_kategori.id_kategori = tbl_arsip.id_kategori', 'left')
-        ->where('id_arsip',$data['id_arsip'])
+        ->where('id_arsip',['id_arsip'])
         ->get()
         ->getRowArray();
     }
@@ -29,19 +31,5 @@ class Model_arsip extends Model
     public function add($data)
     {
         $this->db->table('tbl_arsip')->insert($data);
-    }
-
-    public function edit($data)
-    {
-        $this->db->table('tbl_arsip')
-            ->where('id_arsip', $data['id_arsip'])
-            ->update($data);
-    }
-
-    public function delete_data($data)
-    {
-        $this->db->table('tbl_arsip')
-            ->where('id_arsip', $data['id_arsip'])
-            ->delete($data);
     }
 }
