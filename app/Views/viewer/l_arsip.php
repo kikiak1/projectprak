67ec591a1f8cafd60d603e817fdedad1f1ef16f4
FathurRahmanRais <83505914+FathurRahmanRais@users.noreply.github.com>
2022-11-22 21:45:53 +0700

    Add files via upload

---
+++ app/Views/viewer/l_arsip.php
@@ -0,0 +1,64 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="box box-primary box-solid">
+            <div class="box-header with-border">
+                <h3 class="box-title">Data Arsip</h3>
+
+                <div class="box-tools pull-right">
+                    <a href="<?= base_url('arsipviewer/add/')?>" class="btn btn-primary btn-sm btn-flat">
+                        <i class="fa fa-plus"></i> Add</a>
+                </div>
+
+            </div>
+
+            <div class="box-body">
+                
+                <?php
+                if (session()->getFlashdata('pesan')) {
+                    echo '<div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <i class="icon fa fa-check"></i> Success! - ';
+                    echo session()->getFlashdata('pesan');
+                    echo '</div>';
+                }
+                ?>
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th width="80px">No</th>
+                            <th>No Arsip</th>
+                            <th>Nama Arsip</th>
+                            <th>Kategori</th>
+                            <th>Upload</th>
+                            <th>Update</th>
+                            <th>User</th>
+                            <th>Departemen</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1;
+                        foreach ($arsip as $key => $value) { ?>
+                        <tr>
+                            <td><?= $no++; ?></td>
+                            <td><?= $value['no_arsip']; ?></td>
+                            <td><?= $value['nama_arsip']; ?></td>
+                            <td><?= $value['nama_kategori']; ?></td>
+                            <td><?= $value['tgl_upload']; ?></td>
+                            <td><?= $value['tgl_update']; ?></td>
+                            <td><?= $value['nama_user']; ?></td>
+                            <td><?= $value['nama_dep']; ?></td>
+                            <td class="text-center">
+                                <a href="<?= base_url('arsipviewer/viewpdf/' . $value['id_arsip']) ?>">
+                                    <i class="fa fa-file-pdf-o fa-2x label-danger"></i></a><br>
+                                <?= number_format($value['ukuran_file'], 0) ?> Byte
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+    </div>
+</div>
