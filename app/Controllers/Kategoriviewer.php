fe7cad61e29a3dd7775829d641c4c1f73e4c8c87
Teedo_wee <syonotsurugi123@gmail.com>
2022-11-22 21:29:01 +0700

    Update Kategoriviewer.php

--- app/Controllers/Kategoriviewer.php
+++ app/Controllers/Kategoriviewer.php
@@ -13,5 +13,18 @@ class Kategoriviewer extends BaseController
             echo 'Access denied';
             exit;
         }
+        $this->Model_kategori = new Model_kategori();
+        helper('form');
     }
+
+    public function index()
+    {
+        $data = array(
+            'title'    => 'Kategori',
+            'kategori' => $this->Model_kategori->all_data(),
+            'isi'      => 'viewer/l_kategori'
+        );
+        return view('viewer/layout/l_wrapper' ,$data);
+    }
+
 }
