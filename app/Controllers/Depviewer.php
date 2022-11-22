3bca8e112e07a917989fe7c463dab69cc20eb742
Teedo_wee <syonotsurugi123@gmail.com>
2022-11-22 21:28:33 +0700

    Update Depviewer.php

--- app/Controllers/Depviewer.php
+++ app/Controllers/Depviewer.php
@@ -13,5 +13,18 @@ class Depviewer extends BaseController
             echo 'Access denied';
             exit;
         }
+        $this->Model_dep = new Model_dep();
+        helper('form');
     }
+
+    public function index()
+    {
+        $data = array(
+            'title'    => 'Departement',
+            'dep' => $this->Model_dep->all_data(),
+            'isi'      => 'viewer/l_dep'
+        );
+        return view('viewer/layout/l_wrapper' ,$data);
+    }
+
 }
