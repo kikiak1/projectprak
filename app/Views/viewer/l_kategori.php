97d415a789f8ced80aaf2af5f439a90cf384bdb1
Teedo_wee <syonotsurugi123@gmail.com>
2022-11-22 21:27:02 +0700

    Add files via upload

---
+++ app/Views/viewer/l_kategori.php
@@ -0,0 +1,41 @@
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="box box-primary box-solid">
+            <div class="box-header with-border">
+                <h3 class="box-title">Data Kategori</h3>
+
+            </div>
+
+            <div class="box-body">
+            <?php
+            if (session()->getFlashdata('pesan')) {
+                echo '<div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <i class="icon fa fa-check"></i> Success! - ';
+                echo session()->getFlashdata('pesan');
+                echo '</div>';
+            }
+            ?>
+            <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th width="80px">No</th>
+                            <th>Kategori</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1;
+                        foreach ($kategori as $key => $value) { ?>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $value['nama_kategori']; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+    </div>
+</div>
