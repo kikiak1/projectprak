37b75d2bc30b0d76c8c2613e9cf65fdb89d90b7b
Lunix <76925756+MZakaTawakal@users.noreply.github.com>
2022-11-23 00:58:43 +0700

    Add files via upload

---
+++ app/Views/viewer/layout/l_wrapper.php
@@ -0,0 +1,6 @@
+<?php
+echo view('viewer/layout/l_head');
+echo view('viewer/layout/l_header');
+echo view('viewer/layout/l_nav');
+echo view('viewer/layout/l_content');
+echo view('viewer/layout/l_footer');
